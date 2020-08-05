2f8e50bcae6e56e875387bcd35d8aefeec7614d2
gurkan <gydatascience@gmail.com>
2020-08-05 19:37:27 +0300

    First commit

---
+++ first.py
@@ -0,0 +1,6 @@
+
+name = input("Please enter your name: ").title().strip()
+if name == "Gurkan":
+    print("Hello, {name1}! The password is:W@12".format(name1 = name))
+else:
+    print("Hello, {name1}! See you later.".format(name1 = name))
